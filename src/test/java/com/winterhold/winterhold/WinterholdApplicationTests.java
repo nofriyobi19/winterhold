df203c37090c4ee74329e84e5bf252c5241e26bf
Nofri Yobi <nofriyobi52@gmail.com>
2025-01-11 18:36:50 +0700

    initial project

---
+++ src/test/java/com/winterhold/winterhold/WinterholdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.winterhold.winterhold;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WinterholdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
